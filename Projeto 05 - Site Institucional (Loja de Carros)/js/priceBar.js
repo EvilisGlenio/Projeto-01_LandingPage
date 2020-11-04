7f48d96b952989ffc62ea4147af61bd9bbb63692
EvilisGlenio <glenio.developer@gmail.com>
2020-11-04 10:11:35 -0300

    projeto 05
    Update priceBar.js

--- Projeto 05 - Site Institucional (Loja de Carros)/js/priceBar.js
+++ Projeto 05 - Site Institucional (Loja de Carros)/js/priceBar.js
@@ -39,10 +39,36 @@ $(function(){
 
             //Ajustar o formato do preço
             preco_atual = (currentValue/100) * preco_maximo;
+            //auto explicativa
+            preco_atual = formatarPreco(preco_atual);
+            //preço sendo atualizado constatemente
             $('.preco_pesquisa').html('R$'+preco_atual);
             
         }
     })
+
+    function formatarPreco(preco_atual){
+        //preço terá apenas duas casas decimais após a virgula
+        preco_atual = preco_atual.toFixed(2);
+        //Separar o preço em 2 arrays, o antes e o depois da vírgula
+        preco_arr = preco_atual.split('.');
+
+        var novo_preco = formatarTotal(preco_arr);
+
+        return novo_preco;
+    }
+
+    function formatarTotal(preco_arr){
+        if(preco_arr[0] < 1000){
+            return preco_arr[0]+','+preco_arr[1];
+        }else if(preco_arr[0] < 10000){
+            return preco_arr[0][0]+'.'+preco_arr[0].substr(1,preco_arr[0].length)+','+preco_arr[1]; 
+        }else if(preco_arr[0] < 100000){
+            return preco_arr[0][0]+preco_arr[0][1]+'.'+preco_arr[0].substr(2,preco_arr[0].length)+','+preco_arr[1]; 
+        }else if(preco_arr[0] < 1000000){
+            return preco_arr[0][0]+preco_arr[0][1]+preco_arr[0][2]+'.'+preco_arr[0].substr(3,preco_arr[0].length)+','+preco_arr[1]; 
+        }
+    }
 
     function disableTextSelection(){
         $("body").css("-webkit-user-select","none");
